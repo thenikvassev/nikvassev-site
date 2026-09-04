ba4601467f0e7160d1854f8135ab63bf2892f835
thenikvassev <nik@storylinepros.com>
2026-09-04 14:55:11 -0700

    Newsletter About: use outdoor polo portrait

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -123,8 +123,8 @@ export default function NewsletterPage() {
       <section className="nl-about" aria-labelledby="about-nik-heading">
         <div className="nl-about-photo">
           <Image
-            src="/photos/nik-portrait-studio.jpg"
-            alt="Nik Vassev, chest-up studio portrait in a black blazer and glasses."
+            src="/photos/nik-portrait-newsletter.jpg"
+            alt="Nik Vassev smiling outdoors in a light polo and glasses, with tropical palms behind him."
             fill
             sizes="(min-width: 900px) 42vw, 100vw"
             className="nl-about-img"
