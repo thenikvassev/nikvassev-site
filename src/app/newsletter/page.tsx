8f5a203d3c616738a69cd756ef30204acb656605
thenikvassev <nik@storylinepros.com>
2026-09-04 14:09:28 -0700

    Newsletter: drop unused Link import

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import Image from "next/image";
-import Link from "next/link";
 import { NewsletterForm } from "@/components/NewsletterForm";
 import { siteConfig } from "@/lib/site";
 
